2f1e4ec4497d1dfcadb8f4f820c9a418a3f1f940
TonyGlide <162181740+TonyGlide@users.noreply.github.com>
2024-06-07 22:28:39 +0000

    as

--- src/models.py
+++ src/models.py
@@ -4,9 +4,8 @@ db = SQLAlchemy()
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    homeworld = db.Column(db.String(120))
-    planet_residing = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    name = db.Column(db.String(250), nullable=False)
+    home_planet = db.Column(db.Integer, db.ForeignKey('planet.id'))
 
     def __repr__(self):
         return '<Person %r>' % self.name
@@ -16,14 +15,16 @@ class Person(db.Model):
             "id": self.id,
             "name": self.name,
             "homeworld": self.homeworld,
+            "planet_residing": self.planet_residing,
             # do not serialize the password, its a security breach
         }
-    
+
+
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    terrain = db.Column(db.String(120))
-    people_residing = db.relationship('Person', backref='resident', lazy='dynamic')
+    name = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250))
+    homeworld_of = db.relationship('Person', backref='homeworld', lazy='dynamic')
     
     def __repr__(self):
         return '<Planet %r>' % self.name
@@ -33,7 +34,5 @@ class Planet(db.Model):
             "id": self.id,
             "name": self.name,
             "terrain": self.terrain,
-            "people_residing": self.people_residing,
             # do not serialize the password, its a security breach
         }
-    
